192f9975ddfa236d213e9e796e4c4a60107a94c4
ultracoolclub <jeremiah.ashcraft@gmail.com>
2019-06-07 23:42:46 -0400

    Adds message that appears after submit form has been submitted

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-
+var contactSubmit = document.getElementById('contactButton');
+var contactInfo = document.querySelector('form');
+;
+contactSubmit.addEventListener('click', ()=> {
+  contactInfo.textContent = "Thanks. I'll talk to you soon.";
+  contactInfo.style.color = "white";
+  contactInfo.style.fontSize = "4em";
+});
